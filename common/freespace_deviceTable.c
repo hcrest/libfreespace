26966965c15865fbdf43a14156fee33f8809c12e
Mehrdad Hassani <Mehrdad.Hassani@hillcrestlabs.com>
2013-04-15 16:04:12 -0400

    Added RC520 to the device table.

--- common/freespace_deviceTable.c
+++ common/freespace_deviceTable.c
@@ -89,6 +89,7 @@ const struct FreespaceDeviceAPI freespace_deviceAPITable[] = {
 	{ 0x1d5a, 0xc107, 1, 1, 0, {{0, 0},      {0, 0}}, "USB Bootloader for FSP262"},
     { 0x1d5a, 0xc200, 1, 1, 0, {{0, 0},      {0, 0}}, "USB Bootloader for FSP275"},
     { 0x1d5a, 0xc201, 1, 1, 0, {{0, 0},      {0, 0}}, "USB Bootloader for FSP276"},
+	{ 0x1d5a, 0xc2b3, 1, 1, 2, {{4, 0xff01}, {0, 0}}, "Smart USB RF Transceiver v2 (MKCV)"},
 	{ 0x1d5a, 0xc300, 1, 1, 0, {{0, 0},      {0, 0}}, "USB Bootloader for WP160"},
 	{ 0x1d5a, 0xc301, 1, 1, 0, {{0, 0},      {0, 0}}, "USB Bootloader for WP260"},
 	{ 0x1d5a, 0xc302, 1, 1, 0, {{0, 0},      {0, 0}}, "USB Bootloader for S2U160"},
